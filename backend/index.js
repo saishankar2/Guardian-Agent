a36c7f0ec665a26e654d3bf0918c1f2c869fba31
saishankar2 <shankarpatibandla22@gmail.com>
2025-11-20 15:10:11 +0530

    backend-fixes

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,3 @@
-if (typeof global.DOMMatrix === 'undefined') {
-    global.DOMMatrix = class DOMMatrix {
-        constructor() {
-            this.a = 1; this.b = 0; this.c = 0; this.d = 1; this.e = 0; this.f = 0;
-        }
-        toString() { return "matrix(1, 0, 0, 1, 0, 0)"; }
-    };
-}
-
 const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
